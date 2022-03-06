6d58719999a6f144e0c66f457de2a00d7c84c99d
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-03-06 14:29:31 -0300

    Implemented new charactersCard layout and a 404 error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { CharacterComponent } from './views/character/character.component';
 import { CharacterResolver } from './views/character/guards/character.resolver';
 import { CharactersComponent } from './views/characters/characters.component';
 import { CharactersResolver } from './views/characters/guards/characters.resolver';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: CharactersComponent, resolve:{gameSettings:CharactersResolver}},
@@ -13,6 +13,7 @@ const routes: Routes = [
     { path:':id', component: CharacterComponent, resolve: {character:CharacterResolver}},
   ]},
   { path:'', pathMatch:'full', redirectTo: 'dashboard' },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ];
 
 @NgModule({
