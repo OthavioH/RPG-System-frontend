6e1bc915f0477a44b876fe3f24bc0c248b14a650
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-03-07 15:17:48 -0300

    Changed animations cooldowns

--- src/app/views/common/dice-roll-animation/dice-roll-animation.component.ts
+++ src/app/views/common/dice-roll-animation/dice-roll-animation.component.ts
@@ -9,7 +9,7 @@ import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
     trigger('diceResultFadeIn', [
       state('invisible', style({opacity: 0})),
       state('visible', style({opacity: 1})),
-      transition('invisible => visible', animate('500ms')),
+      transition('invisible => visible', animate('950ms')),
       transition('visible => invisible', animate('0ms')),
     ])
   ]
@@ -36,7 +36,7 @@ export class DiceRollAnimationComponent implements OnInit {
     const interval = setTimeout(() => {
       this.animationState = this.animationState === 'invisible' ? 'visible' : 'invisible';
       clearInterval(interval);
-    }, 400);
+    }, 650);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
