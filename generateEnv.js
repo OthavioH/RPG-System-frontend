2cc0971c29cd8526e9a7b99bc25c10ca28030ff3
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-03-09 19:10:00 -0300

    Changed json path

--- generateEnv.js
+++ generateEnv.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const envData = {apiUrl: process.env.apiUrl};
 const envDataJSON = JSON.stringify(envData);
 
-console.log(`${__dirname}/src/envionments`);
+console.log(`${__dirname}/src/environments`);
 
-fs.writeFile(`${__dirname}/src/envionments`,envDataJSON,'utf8', (err)=>{
+fs.writeFile(`${__dirname}/src/environments/env.json`,envDataJSON,'utf8', (err)=>{
     if (err) {
         return console.error(err);
     }
