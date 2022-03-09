18898ae1f18794a0faca58163927cd01bd40fbcd
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-03-09 18:40:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const appName = 'rpg-system';
 const envData = {apiUrl: process.env.apiUrl};
 const envDataJSON = JSON.stringify(envData);
 
+console.log(__dirname);
+
 fs.writeFile(`${__dirname}/dist/src/envionments`,envDataJSON,'utf8', (err)=>{
     if (err) {
         return console.error(err);
