d17374d13942fe1767d3025e2fcb247a7daddd01
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-03-09 18:31:44 -0300

    changed json creating path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const appName = 'rpg-system';
 const envData = {apiUrl: process.env.apiUrl};
 const envDataJSON = JSON.stringify(envData);
 
-fs.writeFile('env.json',envDataJSON,'utf8', (err)=>{
+fs.writeFile(`${__dirname}/dist/src/envionments`,envDataJSON,'utf8', (err)=>{
     if (err) {
         return console.error(err);
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import envVars from './../../env.json';
+import envVars from './env.json';
 
 export const environment = {
   production: false,
