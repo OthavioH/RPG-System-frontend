ee6f35b6e253b36431d420202b150129610d9cbb
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-08-02 13:33:54 -0300

    Changed HTML lang to pt-br

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rpg-system';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+    this.document.documentElement.lang = 'pt-br';
+  }
 }
