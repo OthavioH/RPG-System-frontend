6e915765ff4dcd958add59f4d0ec065b0e62d7ab
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-11-28 19:34:04 -0300

    fixing apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'ec2-18-231-191-4.sa-east-1.compute.amazonaws.com',
+  apiUrl: 'https://ec2-18-231-191-4.sa-east-1.compute.amazonaws.com',
   frontendUrl:'https://firexter-rpg-system-frontend.herokuapp.com',
 };
