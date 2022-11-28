040db8aead05a332b725a2abe0a77078d848301d
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-11-28 19:45:24 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ec2-18-231-191-4.sa-east-1.compute.amazonaws.com:3333',
+  apiUrl: 'http://ec2-18-231-191-4.sa-east-1.compute.amazonaws.com:3333',
   frontendUrl:'https://firexter-rpg-system-frontend.herokuapp.com',
 };
