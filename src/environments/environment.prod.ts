e0a0fb0c09b48c66b1db2fcd5c922e2925ca9fbb
Othavio Henrico <46772924+OthavioH@users.noreply.github.com>
2022-12-01 04:40:23 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rpg-system-backend.onrender.com',
+  apiUrl: 'http://18.230.22.87:8080',
   frontendUrl:'https://rpg-system-frontend.vercel.app/',
 };
